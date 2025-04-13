ecd3c4403d2b44a11e882f491d87837b3c6b9d34
digiforgedynamics <DigiforgeDynamics@gmail.com>
2025-04-13 20:37:09 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,20 @@
 
+  import { auth } from './firebase-config.js';
+import { signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/11.6.0/firebase-auth.js";
+
+const loginForm = document.getElementById('login-form');
+const errorMessage = document.getElementById('error-message');
+
+loginForm.addEventListener('submit', (e) => {
+  e.preventDefault();
+  const email = document.getElementById("email").value;
+  const password = document.getElementById("password").value;
+
+  signInWithEmailAndPassword(auth, email, password)
+    .then(() => {
+      window.location.href = "dashboard.html";
+    })
+    .catch((error) => {
+      errorMessage.textContent = `Error: ${error.message}`;
+    });
+});
