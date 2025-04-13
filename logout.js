bbcc5af5e24fbf5bbd3367a7775007c6538ae52c
digiforgedynamics <DigiforgeDynamics@gmail.com>
2025-04-13 21:56:35 +0530

    Update logout.js

--- logout.js
+++ logout.js
@@ -1,8 +1,12 @@
-  import { auth } from './firebase-config.js';
+// logout.js
 import { signOut } from "https://www.gstatic.com/firebasejs/11.6.0/firebase-auth.js";
+import { auth } from "./firebase.js";
 
 document.getElementById("logout-btn").addEventListener("click", () => {
   signOut(auth).then(() => {
     window.location.href = "index.html";
+  }).catch((error) => {
+    console.error("Logout error:", error);
+    alert("Logout failed. Try again.");
   });
 });
