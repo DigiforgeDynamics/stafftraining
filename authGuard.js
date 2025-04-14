cfeae23e6629845b9b08524a07c005e83c096b5a
digiforgedynamics <DigiforgeDynamics@gmail.com>
2025-04-14 09:03:27 +0530

    Update authGuard.js

--- authGuard.js
+++ authGuard.js
@@ -1,9 +1,24 @@
-// authGuard.js
-import { onAuthStateChanged } from "https://www.gstatic.com/firebasejs/11.6.0/firebase-auth.js";
-import { auth } from "./firebase.js";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/11.6.0/firebase-app.js";
+import { getAuth, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/11.6.0/firebase-auth.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCQ3HyXaWZ58fMJxNOt2TpjDf5X0QsEZxo",
+  authDomain: "stafftraining-eef33.firebaseapp.com",
+  projectId: "stafftraining-eef33",
+  storageBucket: "stafftraining-eef33.appspot.com",
+  messagingSenderId: "912734544923",
+  appId: "1:912734544923:web:0d52ee5deb49e6380a04be",
+  measurementId: "G-3XY8E8XVT2"
+};
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
 
 onAuthStateChanged(auth, (user) => {
   if (!user) {
     window.location.href = "index.html";
+  } else {
+    // Allow content to show
+    document.body.classList.remove("loading");
   }
 });
