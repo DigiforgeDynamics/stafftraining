6095db75e655938eb5c6a4fbec1372f6612a8ce1
digiforgedynamics <DigiforgeDynamics@gmail.com>
2025-04-14 09:02:59 +0530

    Update logout.js

--- logout.js
+++ logout.js
@@ -1,17 +1,21 @@
-// logout.js
-import { signOut } from "https://www.gstatic.com/firebasejs/11.6.0/firebase-auth.js";
-import { auth } from "./firebase.js";
+import { getAuth, signOut } from "https://www.gstatic.com/firebasejs/11.6.0/firebase-auth.js";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/11.6.0/firebase-app.js";
 
-document.addEventListener("DOMContentLoaded", () => {
-  const logoutBtn = document.getElementById("logout-btn");
-  if (logoutBtn) {
-    logoutBtn.addEventListener("click", () => {
-      signOut(auth).then(() => {
-        window.location.href = "index.html";
-      }).catch((error) => {
-        alert("Error logging out.");
-        console.error(error);
-      });
-    });
-  }
+const firebaseConfig = {
+  apiKey: "AIzaSyCQ3HyXaWZ58fMJxNOt2TpjDf5X0QsEZxo",
+  authDomain: "stafftraining-eef33.firebaseapp.com",
+  projectId: "stafftraining-eef33",
+  storageBucket: "stafftraining-eef33.appspot.com",
+  messagingSenderId: "912734544923",
+  appId: "1:912734544923:web:0d52ee5deb49e6380a04be",
+  measurementId: "G-3XY8E8XVT2"
+};
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+
+document.getElementById("logout-btn").addEventListener("click", () => {
+  signOut(auth).then(() => {
+    window.location.href = "index.html";
+  });
 });
