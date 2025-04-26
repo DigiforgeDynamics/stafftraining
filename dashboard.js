ca858793a9095f887f5c7c3369e8b93ab906c718
digiforgedynamics <DigiforgeDynamics@gmail.com>
2025-04-26 19:48:22 +0530

    Update dashboard.js

--- dashboard.js
+++ dashboard.js
@@ -1,4 +1,30 @@
-  document.querySelectorAll(".collapsible-header").forEach((button) => {
+import { getAuth, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/11.6.0/firebase-auth.js";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/11.6.0/firebase-app.js";
+
+// Firebase config (make sure it's the same as in your other files)
+const firebaseConfig = {
+  apiKey: "AIzaSyCQ3HyXaWZ58fMJxNOt2TpjDf5X0QsEZxo",
+  authDomain: "stafftraining-eef33.firebaseapp.com",
+  projectId: "stafftraining-eef33",
+  storageBucket: "stafftraining-eef33.appspot.com",
+  messagingSenderId: "912734544923",
+  appId: "1:912734544923:web:0d52ee5deb49e6380a04be",
+  measurementId: "G-3XY8E8XVT2"
+};
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+
+// Check user authentication state
+onAuthStateChanged(auth, (user) => {
+  if (!user) {
+    // If no user is authenticated, redirect to the login page (index.html)
+    window.location.href = "index.html";
+  }
+});
+    
+
+document.querySelectorAll(".collapsible-header").forEach((button) => {
   button.addEventListener("click", () => {
     const content = button.nextElementSibling;
     content.style.display =
@@ -31,3 +57,5 @@ function updateProgressBar() {
 }
 
 updateProgressBar();
+
+
