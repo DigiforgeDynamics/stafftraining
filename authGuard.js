a2b3f0fdf7d6d0ec0ac08e891b58b877e8347d93
digiforgedynamics <DigiforgeDynamics@gmail.com>
2025-04-26 17:36:56 +0530

    Update authGuard.js

--- authGuard.js
+++ authGuard.js
@@ -6,9 +6,8 @@ import { getDatabase, ref, get } from "https://www.gstatic.com/firebasejs/11.6.0
 const firebaseConfig = {
   apiKey: "AIzaSyCQ3HyXaWZ58fMJxNOt2TpjDf5X0QsEZxo",
   authDomain: "stafftraining-eef33.firebaseapp.com",
-  databaseURL: "https://stafftraining-eef33-default-rtdb.asia-southeast1.firebasedatabase.app",
   projectId: "stafftraining-eef33",
-  storageBucket: "stafftraining-eef33.firebasestorage.app",
+  storageBucket: "stafftraining-eef33.appspot.com",
   messagingSenderId: "912734544923",
   appId: "1:912734544923:web:0d52ee5deb49e6380a04be",
   measurementId: "G-3XY8E8XVT2"
@@ -29,7 +28,7 @@ onAuthStateChanged(auth, (user) => {
 
   if (user) {
     // User is authenticated, check user role
-    const userRef = ref(db, 'users/' + user.uid);  // Get the user's data from Realtime DB
+    const userRef = ref(db, 'users/' + user.uid);  // Get the user's data
     get(userRef).then((snapshot) => {
       if (snapshot.exists()) {
         const userData = snapshot.val();
